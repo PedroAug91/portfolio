c17d86b9e54c9ab48771141f18be3bf1dc7b74f7
Pedro Augusto <pedro.aug2808@gmail.com>
2025-07-28 15:34:53 -0300

    feat: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,37 @@
+import { GithubIcon, LinkedInIcon, GmailIcon } from "./Icons";
+
 function Footer() {
-    return <footer></footer>;
+    return (
+        <footer className='border-t border-t-neutral-200 py-9 px-3'>
+            <div className='flex flex-col gap-4'>
+                <div className='flex flex-col gap-2'>
+                    <h1 className='text-2xl'>Contato</h1>
+                </div>
+                <nav className='flex flex-col gap-4'>
+                    <a
+                        target='_blank'
+                        href='https://github.com/pedroaug91'
+                        className='text-sm text-black/75 flex gap-2'
+                    >
+                        <GithubIcon />
+                        <span>PedroAug91</span>
+                    </a>
+                    <a
+                        target='_blank'
+                        href='https://www.linkedin.com/in/pedro-augusto-29812229b'
+                        className='text-sm text-black/75 flex gap-2'
+                    >
+                        <LinkedInIcon />
+                        <span>Pedro Augusto</span>
+                    </a>
+                    <span className='text-sm text-black/75 flex gap-2'>
+                        <GmailIcon />
+                        <span>pedro.aug2808@gmail.com</span>
+                    </span>
+                </nav>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -74,6 +74,21 @@ function ChevronLeft({ className }: IconProps) {
     );
 }
 
+function GmailIcon({ className }: IconProps) {
+    return (
+        <svg
+            viewBox='0 0 20 16'
+            xmlns='http://www.w3.org/2000/svg'
+            className={`h-5 w-5 ${className ? className : ""}`}
+        >
+            <path
+                d='M18,0 L2,0 C0.9,0 0,0.9 0,2 L0,14 C0,15.1 0.9,16 2,16 L18,16 C19.1,16 20,15.1 20,14 L20,2 C20,0.9 19.1,0 18,0 L18,0 Z M18,14 L16,14 L16,5.2 L10,9 L4,5.2 L4,14 L2,14 L2,2 L3.2,2 L10,6.2 L16.8,2 L18,2 L18,14 L18,14 Z'
+                id='Shape'
+            />
+        </svg>
+    );
+}
+
 export {
     GithubIcon,
     LinkedInIcon,
@@ -81,4 +96,5 @@ export {
     MenuIcon,
     ChevronRight,
     ChevronLeft,
+    GmailIcon,
 };
